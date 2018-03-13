832922e897e4e091d0655439d81cff56aaa2db79
Cristian <cristianqs31@gmail.com>
2018-03-13 17:11:20 +0000

    Paginación con Búsqueda Done

--- js/displayer.js
+++ js/displayer.js
@@ -1,17 +1,21 @@
-var currentPage = 0;
-
+var currentPage = 1;
+var previousPage = 0;
+var lastPage = 0;
+var apartamentsAll = [];
 loadApartments().then(hotels => {
-    showApartment(hotels);
-})
+    previousPage = 0;
+    lastPage = Math.trunc((hotels.length / 15) - 1);
+    apartamentsAll = hotels;
+    showApartment(apartamentsAll);
+    choosePage();
+});
 
-function showApartment(apartaments) {
-    var apartamentsPage = apartaments.slice(currentPage*15, (currentPage+1)*15);
+function showApartment(hotels) {
+    apartamentsPage = hotels.slice(currentPage * 15, (currentPage + 1) * 15);
     for (let i = 0; i < apartamentsPage.length; i++) {
         createApartmentHtml(apartamentsPage[i]);
     }
-    createPagination();
 }
-
 
 function createApartmentHtml(apartament) {
     var Image = apartament["Hotel Image"];
@@ -29,38 +33,57 @@ function createApartmentHtml(apartament) {
 `);
 }
 
-function createPagination() {
-    var html = `<li class="page-item"><a class="page-link" onclick="prevPage()">Previous</a></li>
-        <li class="page-item"><a class="page-link" href="#"  onclick="goPage(event, ${currentPage})">${currentPage}</a></li>
-        <li class="page-item"><a class="page-link" href="#"  onclick="goPage(event, ${currentPage+1})">${currentPage+1}</a></li>
-        <li class="page-item"><a class="page-link" href="#"  onclick="goPage(event, ${currentPage+2})">${currentPage+2}</a></li>
-        <li class="page-item"><a class="page-link" href="#" onclick="nextPage()">Next</a></li>`;
-    $("#pages").append(html);
+function choosePage() {
+    console.log(previousPage);
+    console.log(lastPage);
+    createPagination(currentPage);
 }
 
-function prevPage() {
-    currentPage = currentPage-1;
-    saveActualPage();
+function createPagination(page) {
+    $("#pages").append(`<li class="page-item"><a class="page-link" href="#" onclick="prevPage()">Previous</a></li>`);
+    if (previousPage != lastPage) {
+        var difference = lastPage - previousPage;
+        if (difference <= 3) {
+            for (let i = 0; i < difference; i++) {
+                $("#pages").append(`<li class="page-item"><a class="page-link" href="#"  onclick="goPage(${i})">${i}</a></li>`);
+            }
+        } else {
+            if (currentPage + 1 == lastPage || currentPage == lastPage ) {
+                $("#pages").append(`<li class="page-item"><a class="page-link" href="#"  onclick="goPage(${previousPage})">${previousPage}</a></li>`);
+                var html = `<li class="page-item"><a class="page-link" href="#" > ... </a></li>
+                <li class="page-item"><a class="page-link" href="#"  onclick="goPage(${page - 1})">${page - 1}</a></li>
+                <li class="page-item"><a class="page-link" href="#"  onclick="goPage(${page})">${page}</a></li>`;
+                $("#pages").append(html);
+                if(currentPage + 1 == lastPage ) $("#pages").append(`<li class="page-item"><a class="page-link" href="#"  onclick="goPage(${lastPage})">${lastPage}</a></li>`);
+
+            } else if (currentPage - 1 == previousPage || currentPage == previousPage) {
+                if(currentPage - 1 == previousPage) $("#pages").append(`<li class="page-item"><a class="page-link" href="#"  onclick="goPage(${previousPage})">${previousPage}</a></li>`);
+                var html = `
+                <li class="page-item"><a class="page-link" href="#"  onclick="goPage(${page})">${page}</a></li>
+                <li class="page-item"><a class="page-link" href="#"  onclick="goPage(${page + 1})">${page + 1}</a></li>
+                <li class="page-item"><a class="page-link" href="#" > ... </a></li>`;
+                $("#pages").append(html);
+                $("#pages").append(`<li class="page-item"><a class="page-link" href="#"  onclick="goPage(${lastPage})">${lastPage}</a></li>`);
+            } else {
+                var html = `<li class="page-item"><a class="page-link" href="#" > ... </a></li>
+                <li class="page-item"><a class="page-link" href="#"  onclick="goPage(${page - 1})">${page - 1}</a></li>
+                <li class="page-item"><a class="page-link" href="#"  onclick="goPage(${page})">${page}</a></li>
+                <li class="page-item"><a class="page-link" href="#"  onclick="goPage(${page + 1})">${page + 1}</a></li>
+                <li class="page-item"><a class="page-link" href="#" > ... </a></li>`;
+                
+                $("#pages").append(`<li class="page-item"><a class="page-link" href="#"  onclick="goPage(${previousPage})">${previousPage}</a></li>`);
+                $("#pages").append(html);
+                $("#pages").append(`<li class="page-item"><a class="page-link" href="#"  onclick="goPage(${lastPage})">${lastPage}</a></li>`);
+            }
+
+        }
+    }
+    $("#pages").append(`<li class="page-item"><a class="page-link" href="#" onclick="nextPage()">Next</a></li>`);
 }
 
-function nextPage () {
-    currentPage = currentPage +1;
-    saveActualPage();
-}
 
-function goPage(event, idPage) {
-    currentPage=idPage;
-    saveActualPage();
-}
 
-function saveActualPage() {
-    cleanPage();
-    loadApartments().then(hotels => {
-        showApartment(hotels);
-    })
-}
 
-function cleanPage() {
-    $("#pages").children().remove();
-    $("#apartaments").children().remove();
-}
+
+
+
--- js/events.js
+++ js/events.js
@@ -1,6 +1,52 @@
+function searchHotel() {
+    var name = $("#nameSearch").val();
+    loadApartments().then(hotels => {
+        apartamentsAll = hotels.filter(hotels => hotels["Hotel Image"].alt.toLowerCase() === name.toLowerCase());
+        if (apartamentsAll.length != 0) {
+            lastPage = Math.trunc((apartamentsAll.length / 15) - 1);
+            cleanPage();
+            currentPage = 0;
+            showApartment(apartamentsAll);
+            choosePage();
+        } else {
+            lastPage = Math.trunc((hotels.length / 15) - 1);
+            cleanPage();
+            apartamentsAll = hotels;
+            showApartment(apartamentsAll);
+            choosePage();
+        }
+    })
+}
 
+function cleanPage() {
+    $("#pages").children().remove();
+    $("#apartaments").children().remove();
+}
 
-function goPage() {
+function prevPage() {
+    if (currentPage != previousPage) {
+        currentPage = currentPage - 1;
+        saveActualPage();
+    }
+}
+
+function nextPage() {
+    if (currentPage != lastPage) {
+        currentPage = currentPage + 1;
+        saveActualPage();
+    }
+}
+
+function goPage(idPage) {
+    currentPage = idPage;
+    saveActualPage();
+}
+
+function saveActualPage() {
+    cleanPage();
+
+    showApartment(apartamentsAll);
+    choosePage();
 
 }
 
